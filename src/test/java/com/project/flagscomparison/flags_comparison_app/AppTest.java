5c2329e0c88fe8fe5e3100f8c46703dcdd16aa6d
Santiago Velez <108843851+svelezpernett98@users.noreply.github.com>
2024-02-06 21:22:47 -0500

    Update AppTest.java
    adding comments

--- src/test/java/com/project/flagscomparison/flags_comparison_app/AppTest.java
+++ src/test/java/com/project/flagscomparison/flags_comparison_app/AppTest.java
@@ -20,24 +20,34 @@ public class AppTest
 		
 		WebDriver driver;
 		BasePage base = new BasePage();
-		
+
+		//Creates workesheet where the data will be stored
 		base.createWorksheet();
-		
+
+		//For each branch from the branches listed in the config.properties file
 		for(String branch : base.getJHBranchesArray()) {
+			//Initializes the WebDriver
 			driver = base.getDriver();
+
+			//Go to Flag management page in PROD environment
 			driver.get(base.getProdUrl());
-			
+
+			//Instance of prodLoginPageObject to access the webElements from this page
 			ProdLoginPage prodLoginPageObject = new ProdLoginPage();
+			
 			//use of explicit wait:
 //			WebElement loginButton = base.wait.until(ExpectedConditions.visibilityOfElementLocated(prodLoginPageObject.userName));
 //			loginButton.sendKeys(base.getUsername());
+			
+			//Login retrieving username and password set in config.properties file through the getUsername(), and getPassword() methods
 			prodLoginPageObject.getUserName(driver).sendKeys(base.getUsername());
 			prodLoginPageObject.getPassWord(driver).sendKeys(base.getPassword());
 			prodLoginPageObject.getLoginbtn(driver).click();
-			
+
+			//Instance of ProdEmployerConfigPageObject to access the webElements from this page, then enter the employer number and the branch to see the flag configurations for that branch
 			ProdEmployerConfigPage ProdEmployerConfigPageObject = new ProdEmployerConfigPage();
 			ProdEmployerConfigPageObject.getEmployerGroupSpan(driver).click();
-			ProdEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys("1420131938");
+			ProdEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys("ProdEmployerNumberGoesHere");
 			ProdEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys(Keys.ENTER);
 			ProdEmployerConfigPageObject.getBranchSpan(driver).click();
 			ProdEmployerConfigPageObject.getBranchInput(driver).sendKeys(branch);
@@ -45,10 +55,11 @@ public class AppTest
 			ProdEmployerConfigPageObject.getProductSpan(driver).click();
 			ProdEmployerConfigPageObject.getProductInput(driver).sendKeys("J");
 			ProdEmployerConfigPageObject.getProductInput(driver).sendKeys(Keys.ENTER);
-			
-//			ProdEmployerConfigPageObject.getTableLenght(driver);
+
+			//getProdHashMapWithValues()  creates a hasmap with the flag name, and its value (Enabled or Disabled)
 			ProdEmployerConfigPageObject.getProdHashMapWithValues(driver);
-			
+
+			//Same process done for PROD, but in STAGE environment
 			driver.get(base.getStageUrl());
 			StageLoginPage StageLoginPageObject = new StageLoginPage();
 			StageLoginPageObject.getUserName(driver).sendKeys(base.getUsername());
@@ -57,7 +68,7 @@ public class AppTest
 			
 			StageEmployerConfigPage StageEmployerConfigPageObject = new StageEmployerConfigPage();
 			StageEmployerConfigPageObject.getEmployerGroupSpan(driver).click();
-			StageEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys("1420131938");
+			StageEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys("stageEmployerNumberGoesHere");
 			StageEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys(Keys.ENTER);
 			StageEmployerConfigPageObject.getBranchSpan(driver).click();
 			StageEmployerConfigPageObject.getBranchInput(driver).sendKeys(branch);
@@ -66,9 +77,9 @@ public class AppTest
 			StageEmployerConfigPageObject.getProductInput(driver).sendKeys("J");
 			StageEmployerConfigPageObject.getProductInput(driver).sendKeys(Keys.ENTER);
 			
-//			StageEmployerConfigPageObject.getTableLenght(driver);
 			StageEmployerConfigPageObject.getStageHashMapWithValues(driver);
-		
+
+			//Same process done for PROD and STAGE, but in TEST environment
 			driver.get(base.getTestUrl());
 			TestLoginPage testLoginPageObject = new TestLoginPage();
 			testLoginPageObject.getUserName(driver).sendKeys(base.getUsername());
@@ -77,7 +88,7 @@ public class AppTest
 			
 			TestEmployerConfigPage testEmployerConfigPageObject = new TestEmployerConfigPage();
 			testEmployerConfigPageObject.getEmployerGroupSpan(driver).click();
-			testEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys("1414258879");
+			testEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys("testEmployerNumberGoesHere");
 			testEmployerConfigPageObject.getEmployerGroupInput(driver).sendKeys(Keys.ENTER);
 			testEmployerConfigPageObject.getBranchSpan(driver).click();
 			testEmployerConfigPageObject.getBranchInput(driver).sendKeys(branch);
@@ -86,13 +97,13 @@ public class AppTest
 			testEmployerConfigPageObject.getProductInput(driver).sendKeys("J");
 			testEmployerConfigPageObject.getProductInput(driver).sendKeys(Keys.ENTER);
 			
-//			testEmployerConfigPageObject.getTableLenght(driver);
 			testEmployerConfigPageObject.getTestHashMapWithValues(driver);
-			
+
+			//compareHashmaps() compares the 3 hasmaps created for each environment, and adds a sheet to the worksheet with the comparison of the values of the flags across the 3 environments, marking which flags are different from what we have in PROD
 			base.compareHashmaps(ProdEmployerConfigPageObject.prodFlagValuelinkedHashMap, StageEmployerConfigPageObject.stageFlagValuelinkedHashMap, testEmployerConfigPageObject.testFlagValuelinkedHashMap);
-			
 			base.addSheet(branch);
-			
+
+			//Closes the window
 			driver.close();
 		}
 		
